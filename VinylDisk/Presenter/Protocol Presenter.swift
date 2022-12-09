0bde29ea81db36c723e478634a67783631fe4bc5
Alexander_Borisov <borisovab@mail.ru>
2022-12-09 14:55:30 +0300

    Added StartView which present ViewController by Coordinator
    Added StartView which present ViewController by Coordinator

--- VinylDisk/Presenter/Protocol Presenter.swift
+++ VinylDisk/Presenter/Protocol Presenter.swift
@@ -8,5 +8,8 @@
 import Foundation
 
 protocol PresenterProtocol {
-    
+    func presentInitialState(state: HomeModels.ResponsePresenter.InitialState)
+    func presentStartButton(isHidden: Bool)
+    func presentStopButton(isHidden: Bool)
+    func presentUpdateSpeed(speed: Float)
 }
